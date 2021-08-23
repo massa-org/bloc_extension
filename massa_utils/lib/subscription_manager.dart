ebf6defc7f0ae23bc0edd633084dd8128432cd80
iliser <i.d.mvmii@gmail.com>
2021-08-23 13:45:46 +0800

    fix: remove unwanted .md files, change repo link and docs

--- massa_utils/lib/subscription_manager.dart
+++ massa_utils/lib/subscription_manager.dart
@@ -2,8 +2,8 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 
-/// Listen stream and call update on event
-/// on dispose or close must call closeManager
+/// Listen stream and call update on each event
+/// must call closeManager on dispose or close
 mixin StreamSubscriptionManager {
   final List<StreamSubscription> _sub = [];
 
@@ -16,19 +16,23 @@ mixin StreamSubscriptionManager {
   }
 }
 
-/// Listen stream or bloc and call update on event
-/// on dispose or close must call closeManager
+/// Listen stream or bloc and call update on each event
+/// must call closeManager on dispose or close
 mixin BlocSubscriptionManager {
   final List<StreamSubscription> _sub = [];
 
+  /// listen stream and call update but can't get access to event value :[
   void listenStreams(Iterable<Stream> streams, void Function() update) {
     _sub.addAll(streams.map((e) => e.listen((_) => update())));
   }
 
+  /// listen blocs and call update
+  /// can add multiple listeners and can be called multiple times with different arguments
   void listenBlocs(Iterable<BlocBase> cubits, void Function() update) {
     listenStreams(cubits.map((e) => e.stream), update);
   }
 
+  // Cancel all subscriptions
   Future<void> closeManager() async {
     await Future.wait(_sub.map((e) => e.cancel()));
   }
--- readme.md
+++ readme.md
@@ -1,7 +1,1 @@
 Just a pile of packages
-TODO
-- [x] fill pubspec.yaml, packages versions etc
-- [ ] brainstorm package names
-- [ ] change internal dependencies to git dependencies
-- [ ] add description to all packages
-- [ ] add tests
